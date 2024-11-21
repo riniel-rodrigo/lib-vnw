3bec2597a310221445d9d9dd9cafda4dcd8ef116
Riniel <rinielrodrigo555@gmail.com>
2024-11-21 20:21:02 -0300

    :sparkles: feat: create header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,31 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { IoSearchSharp } from "react-icons/io5";
+
+import logo from '../../assets/logo.svg';
+
+import s from './header.module.scss';
 
 export default function Header() {
+    const { pathname } = useLocation();
+
     return (
-        <>
-            <h1>Header</h1>
-            <Link to="/">Home</Link>
-            <Link to="/livros-doados">Livros doados</Link>
-            <Link to="/quero-doar">Quero doar</Link>
-        </>
+        <header className={s.header}>
+
+            <Link to="/" className={s.boxLogo}>
+                <img src={logo} alt="logo - Livro aberto" />
+                <h1>vnw.Livros</h1>
+            </Link>
+
+            <nav className={s.nav}>
+                <Link className={pathname === '/' ? `${s.isHere}` : ''} to="/">Home</Link>
+                <Link className={pathname === '/livros-doados' ? `${s.isHere}` : ''} to="/livros-doados">Livros Doados</Link>
+                <Link className={pathname === '/quero-doar' ? `${s.isHere}` : ''} to="/quero-doar">Quero Doar</Link>
+            </nav>
+
+            <div className={s.search}>
+                <input type="text" placeholder='O que você procura?' />
+                <IoSearchSharp />
+            </div>
+        </header>
     )
 }
