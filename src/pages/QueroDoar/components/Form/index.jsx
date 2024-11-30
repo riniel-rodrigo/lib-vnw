30f461ada2daff1ec5b47d6226345723a3358aa6
Riniel <rinielrodrigo555@gmail.com>
2024-11-30 20:14:20 -0300

    :iphone: feat: responsividade

--- src/pages/QueroDoar/components/Form/index.jsx
+++ src/pages/QueroDoar/components/Form/index.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { HiOutlineBookOpen } from "react-icons/hi";
 
 import Title from "../../../../components/Title";
@@ -7,24 +6,22 @@ import * as s from "./form.module.scss";
 
 export default function Form() {
   return (
-    <>
-      <form className={s.Form}>
-        <div className={s.FormHeader}>
-          <HiOutlineBookOpen size={40} color="#005695" />
-          <Title text="Informações do Livro" size={25} />
-        </div>
+    <form className={s.Form}>
+      <div className={s.FormHeader}>
+        <HiOutlineBookOpen size={40} color="#005695" />
+        <Title text="Informações do Livro" size={25} />
+      </div>
 
-        <div className={s.FormGroup}>
-          <input type="text" placeholder="Título" />
-          <input type="" placeholder="Categoria" />
-          <input type="text" placeholder="Autor" />
-          <input type="url" placeholder="Link da imagem" />
-        </div>
+      <div className={s.FormGroup}>
+        <input type="text" placeholder="Título" />
+        <input type="" placeholder="Categoria" />
+        <input type="text" placeholder="Autor" />
+        <input type="url" placeholder="Link da imagem" />
+      </div>
 
-        <div className={s.FormButton}>
-          <button disabled>Doar</button>
-        </div>
-      </form>
-    </>
+      <div className={s.FormButton}>
+        <button disabled>Doar</button>
+      </div>
+    </form>
   );
 }
